c434bb432283f7bdf0581b93e575073035ba6e72
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-12-09 09:50:14 +0700

    Update Item.h

--- Item.h
+++ Item.h
@@ -1,14 +1,27 @@
 #pragma once
 #include <Windows.h>
 #include <iostream>
+#include "CommonFunction.h"
 using namespace std;
+const int NUM_CATORY_ITEM = 4;
+enum catoryItem
+{
+	x2 = 0,
+	x0_5 = 1,
+	add10 = 2,
+	sub10 = 3
+};
+const static string catory[NUM_CATORY_ITEM] = { "x2", "x0.5","+10", "-10" };
 class Item
 {
 private:
 	int _x, _y; // Toa do (x, y) hien tai cua qua Item
 	int _ox, _oy; // Toa do (x, y) cu cua qua Item
 	int _vx, _vy; // Van toc cua qua Item
+	int cat;
+	bool is_move;
 public:
+	
 	//getter & setter
 	void SetX(int x) { _x = x; }
 	void SetY(int y) { _y = y; }
@@ -27,11 +40,22 @@ public:
 
 	int VX() { return _vx; }
 	int VY() { return _vy; }
+
+	bool getIsMove() { return is_move; }
+	void setIsMove(bool val) { is_move = val; }
+
+	int getCatory() { return cat; }
+	void setCatory(int val) { cat = val; }
+
+	void resetCatoryRandom();
+
 public:
 	Item();
 	~Item();
 	Item(int, int); // Khoi tao
 	void Draw(HANDLE&); // Ve qua bong
+	void move();
 
+	void deleteItem(HANDLE& h);
 };
 
