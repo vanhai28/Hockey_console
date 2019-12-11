984beb99a1bf217cf87a8a488765ace015573029
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-12-11 14:19:41 +0700

    Update CommonFunction.h

--- CommonFunction.h
+++ CommonFunction.h
@@ -1,14 +1,21 @@
+// File Chua cac ham dung chung cho nhieu doi tuong trong chuong trinh
 #pragma once
 #include <string>
 #include <Windows.h>
 
 using namespace std;
 
-
+//tao gia tri ngau nhien trong mot khoang cho truoc
+//Tham so start : gia tri khoi dau
+//Tham so end : gia tri ket thuc
+// return : gia tri ngau nhien duoc tao
 int randomRange(int start, int end);
 
 
 //Chuyen mot chuoi sang mot so
+//Tham so line : chuoi can chuyen sang so nguyen
+// return : so duoc chuyen
+//Neu chuyen that bai se tra ve 0
 int tryParseInt(string line);
 
 
