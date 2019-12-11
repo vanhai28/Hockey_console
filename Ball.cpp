13635fccad32c1f3204b76f0122c27eb930614b3
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-12-11 14:25:41 +0700

    Update Ball.cpp

--- Ball.cpp
+++ Ball.cpp
@@ -24,19 +24,12 @@ Ball::Ball(int x, int y) {
 // Chuc nang: Ve qua bong
 void Ball::Draw(HANDLE& h) {
 	SetConsoleTextAttribute(h, 15);
-	COORD c;
-	//Vi tri cu
-	c.X = int(_ox);
-	c.Y = int(_oy);
+	gotoXY(_ox, _oy,h);
 
-	SetConsoleCursorPosition(h, c);
 	//Xoa vi tri cu
 	cout << ' ';
 	//Vi tri moi
-	c.X = int(_x);
-	c.Y = int(_y);
-
-	SetConsoleCursorPosition(h, c);
+	gotoXY(_x, _y, h);
 	SetConsoleTextAttribute(h, 6);
 	//Ve vi tri moi
 	cout <<(char)(233);
@@ -45,4 +38,70 @@ void Ball::Draw(HANDLE& h) {
 	//Gan toa do vi tri moi vao _ox _oy de xu ly tiep khi bong di chuyen
 	_ox = _x;
 	_oy = _y;
+}
+
+
+
+bool Ball:: checkBallCollideBox(vector<Box>& list, int& score)
+{
+	int size = list.size();
+	char maxBox = 3;//Toi da bong chi cham duoc ba Box
+	bool isCollide = false;//kiem tra trang thai va cham
+	bool flag = false;//kiem tra neu dong thoi va cham goc va giua thi uu tien va cham giua
+	Box* p = NULL;//chua dia chi box xay ra va cham goc
+
+	for (int i = 0; i < size; i++)
+	{
+		//va cham o giua
+		if (!list[i].IsBreak() && ((_y == list[i].Y() + 1 && _vy < 0)
+			|| (_y == list[i].Y() - 1 && _vy > 0))
+			&& _x < list[i].X() + list[i].Width() && _x >= list[i].X())
+		{
+			list[i].hideBox();  //xoa box
+			score += list[i].Score();  //tang diem
+			isCollide = true;  //danh dau co va cham
+			_vy *= (- 1);   //doi huong
+			flag = true;   //da co va cham khac va cham goc
+
+		}//va cham hai ben
+		else if (!list[i].IsBreak() && _y == list[i].Y() && (_x == list[i].X() + list[i].Width() || _x == list[i].X() - 1) && _vx!= 0)
+		{
+			list[i].hideBox();//xoa box
+			score += list[i].Score();//tang diem
+			isCollide = true;//danh dau co va cham
+			_vx *= -1;//doi huong
+			flag = true;//da co va cham khac va cham goc
+		}
+		//va cham o goc nhon
+		if (!list[i].IsBreak() && ((_x == list[i].X() + list[i].Width() && _vx < 0) || (_x == list[i].X() - 1 && _vx > 0)) 
+			&&((_y - 1 == list[i].Y() && _vy<0)	||(_y +1 == list[i].Y() && _vy > 0)))
+		{
+			//luu dia chi box bi va cham
+			p = &list[i];
+
+		}
+		//va vao ben trong box
+		if (!list[i].IsBreak() && _y == list[i].Y() && _x < list[i].X() + list[i].Width() && _x >= list[i].X())
+		{
+			list[i].hideBox();//xoa box
+			score += list[i].Score();//tang diem nguoi choi
+			isCollide = true;//danh dau box da bi vo
+			//doi huong bong
+			_vy *= -1;
+			_vx *=  -1;
+			break;
+		}
+	}
+
+	if (!flag && p)//neu chi co va cham goc thi xu li
+	{
+		p->hideBox();//xoa box
+		score += p->Score();//tang diem
+		isCollide = true;//danh dau da va cham
+		//doi huong bong
+		_vy *= -1;
+		_vx *= -1;
+	}
+
+	return isCollide;
 }
