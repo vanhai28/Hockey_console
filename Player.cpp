e4abf7b1bf9033f503a1f09acabd9797093d6328
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-12-11 14:25:59 +0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,10 +1,12 @@
 #include "Player.h"
-
+#include <fstream>
 
 
 bool Player::move()
 {
-	bool isMove = false;
+	bool isMove = false;//khoi tao trang thai di chuyen la false
+
+	//xu li nhan phim di chuyen
 	if (GetAsyncKeyState(VK_LEFT)) {
 		SetX(_x - 2);
 		isMove = true;
@@ -18,15 +20,16 @@ bool Player::move()
 	if (_x < Size() + 1) {
 		SetX(Size() + 1);
 	}
-	else if (_x> (WIDTH_BODER - Size() - 2)) {
+	else if (_x > (WIDTH_BODER - Size() - 2)) {
 		SetX(WIDTH_BODER - Size() - 2);
 	}
-	return isMove ;
+	return isMove;
 }
 
 Player::Player()
 {
 	score = 0;
+	level = 1;
 }
 
 
@@ -35,18 +38,19 @@ Player::~Player()
 }
 
 Player::Player(int x, int y, int size) {
-	_x =x;
+	_x = x;
 	_y = y;
 	_size = size;
 	_ox = x;
 	_oy = y;
 	score = 0;
+	level = 0;
 }
 
 // Input: h(con tro su dung voi man hinh console)
 // Output: Thanh choi
 // Chuc nang: Ve thanh choi
-void Player::Draw(HANDLE& h) 
+void Player::Draw(HANDLE& h)
 {
 	SetConsoleTextAttribute(h, 9);
 	COORD c;
@@ -67,7 +71,7 @@ void Player::Draw(HANDLE& h)
 
 	//Ve vi tri moi khi di chuyen
 	for (int i = -_size; i < _size; ++i) {
-		cout <<(char)(254);
+		cout << (char)(254);
 	}
 
 	SetConsoleTextAttribute(h, 15);
@@ -80,14 +84,11 @@ void Player::getItem(Item item[], int numItem, HANDLE&h)
 {
 	for (int i = 0; i < numItem; i++)
 	{
-		if (item[i].Y() >= _y - 1 && item[i].Y() <= _y && item[i].X()>= _x - _size && item[i].X() < _x + _size) {
-			
-			item[i].deleteItem(h);
-			/*HANDLE h;
-			h = GetStdHandle(STD_OUTPUT_HANDLE);
-			Draw(h);*/
-			item[i].setIsMove(false);
-			
+		if (item[i].Y() >= _y - 1 && item[i].Y() <= _y && item[i].X() >= _x - _size && item[i].X() < _x + _size) {
+
+			item[i].deleteItem(h);//Xoa item tren man hinh
+			item[i].setIsMove(false);//dat trang thai di chuyen cua item ve false
+			//tang diem theo item nhan duoc
 			switch (item[i].getCatory())
 			{
 			case 0:
@@ -103,8 +104,66 @@ void Player::getItem(Item item[], int numItem, HANDLE&h)
 				score -= 10;
 				break;
 			}
-
+			//Loai bo truong hop diem am
 			score = score > 0 ? score : 0;
 		}
+	}
+}
+
+void Player::saveAchievement()
+{
+	fstream f;
+	f.open("achievement.txt", ios::app);//mo file
+
+	if (f.is_open())
+	{
+		//ghi level va diem vao file
+		f << endl << level << endl << score;
+		f.close();//dong file
+	}
+
+}
+
+void Player::showAchievemert(HANDLE h)
+{
+	fstream f;
+	f.open("achievement.txt", ios::in);//mo file
+
+	if (f.is_open())
+	{
+		SetConsoleTextAttribute(h, 9);//dat gia tri cho mau sac chu
+		int i = 6;//tao khoang cach in moi dong trong bang thanh tich
+		string lineScore, lineLevel;
+		gotoXY(WIDTH_BODER + 5, HEIGHT_BODER / 4 + 2, h);
+
+		cout << "ACHIEVEMENT TABLE";
+
+		SetConsoleTextAttribute(h, 10);//dat gia tri cho mau sac chu
+		gotoXY(WIDTH_BODER + 6, HEIGHT_BODER / 4 + 4, h);
+
+		cout << "LEVEL     SCORE";
+
+		while (!f.eof())
+		{
+			gotoXY(WIDTH_BODER + 8, HEIGHT_BODER / 4 + i, h);
+
+			do {
+				getline(f, lineLevel);//doc level tu file
+			} while (lineLevel.empty() && !f.eof());
+			
+			getline(f, lineScore);//doc diem tu file
+			//xuat ra man hinh level va diem
+			cout << lineLevel << "        ";
+			cout << lineScore;
+			i++;
+
+		}
+		f.close();
+	}
+	else
+	{
+		//neu khong doc duoc file thi hien thi thong bao loi
+		gotoXY(WIDTH_BODER + 8, HEIGHT_BODER / 4 + 6, h);
+		cout << "Loi trong qua trinh mo file\n";
 	}
 }
