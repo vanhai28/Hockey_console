320e1b2d4943c1c9cc0b5b8908533a6a4c4562d9
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-12-11 14:25:51 +0700

    Create ConstVariable.h

---
+++ ConstVariable.h
@@ -0,0 +1,8 @@
+#pragma once
+const int TOP_BODER = 6;
+const int LEFT_BODER = 1;
+const int WIDTH_BODER = 55; // Chieu ngang san dau
+const int HEIGHT_BODER = 35;
+const int MAX_LEVEL = 10;
+const int time_play = 2;
+const int seconds = 60;
