4f3ebd02462ac0e3da161a386accd92eb9c44ff5
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-12-15 18:10:08 +0700

    Update File.cpp

--- File.cpp
+++ File.cpp
@@ -3,7 +3,11 @@
 
 void SaveToFile(int scoreplayer, int scorecomputer)
 {
+<<<<<<< Updated upstream
 	
+=======
+
+>>>>>>> Stashed changes
 	fstream savefile;
 	savefile.open("Save.txt", ios::out);
 
@@ -17,6 +21,14 @@ void LoadFile(char mode, int &scoreplayer, int & scorecomputer)
 {
 	fstream loadfile;
 
+<<<<<<< Updated upstream
+=======
+	if (loadfile.fail())
+	{
+		cout << "ban chua luu game" << endl;
+	}
+	list.clear();
+>>>>>>> Stashed changes
 	if (mode == '2')
 	{
 		scoreplayer = 0;
@@ -36,7 +48,16 @@ void LoadFile(char mode, int &scoreplayer, int & scorecomputer)
 		loadfile.close();
 	}
 
+<<<<<<< Updated upstream
 }
+=======
+	while (!loadfile.eof())
+	{
+		do
+		{
+			getline(loadfile, line);
+		} while (line.empty() && !loadfile.eof());
+>>>>>>> Stashed changes
 
 void savebest(int score)
 {
@@ -71,3 +92,31 @@ int Maxscore()
 
 	return max;
 }
+<<<<<<< Updated upstream
+=======
+
+//int Maxscore()
+//{
+//	fstream scorefile;
+//	scorefile.open("Savebest.txt", ios::in);
+//
+//	if (scorefile.fail())
+//	{
+//		cout << "khong mo duoc fine" << endl;
+//		return 0;
+//	}
+//	int max = 0;
+//	string data;
+//	while (scorefile.good())
+//	{
+//		getline(scorefile, data);
+//		if ((int)atoi(data.c_str()) > max)
+//		{
+//			max = (int)atoi(data.c_str());
+//		}
+//	}
+//	scorefile.close();
+//
+//	return max;
+//}
+>>>>>>> Stashed changes
