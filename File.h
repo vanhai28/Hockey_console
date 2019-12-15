9cdd79c88583d51e7f4c45dfe07712f73f07f966
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2019-12-15 17:59:51 +0700

    Update File.h

--- File.h
+++ File.h
@@ -3,6 +3,22 @@
 #include<vector>
 #include<iostream>
 #include<string>
+#include <vector>
+#include "CommonFunction.h"
 using namespace std;
+<<<<<<< Updated upstream
 void SaveToFile(int scoreplayer, int scorecomputer);
 void LoadFile(char mode, int &scoreplayer, int &scorecomputer);
+=======
+
+//luu vao diem cua nguoi choi va may vao file
+void SaveToFile(int scoreplayer, int scorecomputer, vector<playerInfor> listPlayer);
+
+//tai cac diem da duoc luu len
+void LoadFile(char mode, int &scoreplayer, int &scorecomputer, vector<playerInfor> &list);
+
+
+
+////tai diem cao nhat
+//int Maxscore();
+>>>>>>> Stashed changes
